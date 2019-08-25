0394833e17cd8e4866f32ad18c220e76d538cb75
happydhKim <happytime870616@gmail.com>
2019-08-25 20:32:25 +0900

    Fix kakao_search for clean code
    added function connect search

--- Kakao_REST_API/kakao_search.js
+++ Kakao_REST_API/kakao_search.js
@@ -1,19 +1,30 @@
 const axios = require('axios');
 
-axios({
-        method: 'get',
-        url: 'https://dapi.kakao.com/v2/search/web',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Authorization': 'KakaoAK key'
-        },
-        params: {
-            query: '카카오'
-        }
-    })
-    .then(response => {
-        console.log(response.data)
-    })
-    .catch(error => {
-        console.log(error);
-    });
+const METHOD = 'get';
+const URL = 'https://dapi.kakao.com/v2/search/web';
+const HEADERS = {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Authorization': 'KakaoAK key'
+}
+const QUERY = ['카카오','kakao'];
+
+const connectSearch = QUERY => {
+    axios({
+            method: METHOD,
+            url: URL,
+            headers: HEADERS,
+            params: {
+                query: QUERY
+            }
+        })
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+for (let i = 0; i < QUERY.length; i++) {
+    connectSearch(QUERY[i]);
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Crawler_python
-Crawler use python
+# HTTP & REST API
+### HTTP & REST API 두 가지를 이해하자
